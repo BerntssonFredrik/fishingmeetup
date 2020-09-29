1083f00b31aa72b30877f660fcb184744ac00bf5
jensengbg-fredrik-berntsson <fredrikberntsson@hotmail.com>
2020-09-29 21:10:19 +0200

    Actions

--- tests/unit/AddAttendee.spec.js
+++ tests/unit/AddAttendee.spec.js
@@ -1,73 +1,70 @@
-import { shallowMount } from "@vue/test-utils"
-import AddAttendee from "@/components/AddAttendee"
+import { shallowMount } from "@vue/test-utils";
+import AddAttendee from "@/components/AddAttendee";
 
 let event, data;
 beforeEach(() => {
-  event = 
-        {
-            _id: "111",
-            date: "2020-10-30",
-            fish: "Gädda",
-            imgUrl: "lol",
-            info: "Jag har ett extra spö",
-            location: "Delsjön",
-            organizer: "Fredrik Berntsson",
-            time: "11:00",
-            comments: {commentName: "Allan", commentComment: "hej hej"},
-            attendees: {attendeeName: "Allan", AttendeeNr: "123"}
-          },
-          data = {attendee: { attendeeName: "Fred", attendeeNr: "123"}, id: "111"}
+  (event = {
+    _id: "111",
+    date: "2020-10-30",
+    fish: "Gädda",
+    imgUrl: "lol",
+    info: "Jag har ett extra spö",
+    location: "Delsjön",
+    organizer: "Fredrik Berntsson",
+    time: "11:00",
+    comments: { commentName: "Allan", commentComment: "hej hej" },
+    attendees: { attendeeName: "Allan", AttendeeNr: "123" },
+  }),
+    (data = {
+      attendee: { attendeeName: "Fred", attendeeNr: "123" },
+      id: "111",
+    });
+});
 
-})
-
-    it("Should call addAttendee.", async () => {
-      
-      const mockStore = {
-          dispatch: jest.fn(),
-        },
-        wrapper = shallowMount(AddAttendee, {
-          computed: {
-            event: () => {
-              return event;
-            },
-          },
-          propsData:{
-            eventId: "111"
-          },
-          data() {
-            return {
-              newAttendee: { attendeeName: "Fred", attendeeNr: "123"},
-              addAttandee: true
-            }
-          },
-          mocks: { $store: mockStore, },
-        });
-
-      
-      await wrapper.find(".joinButton").trigger("click");
-      await wrapper.vm.$nextTick();
-      expect(mockStore.dispatch).toHaveBeenCalledWith("addAttendee", data);
-    })  
-
- it("should not emit addAttendee if inputs are empty", async () => {
+it("Should call addAttendee.", async () => {
   const mockStore = {
-    dispatch: jest.fn(),
-  },
-  wrapper = shallowMount(AddAttendee, {
-    computed: {
-      event: () => {
-        return event;
+      dispatch: jest.fn(),
+    },
+    wrapper = shallowMount(AddAttendee, {
+      computed: {
+        event: () => {
+          return event;
+        },
       },
+      propsData: {
+        eventId: "111",
+      },
+      data() {
+        return {
+          newAttendee: { attendeeName: "Fred", attendeeNr: "123" },
+          addAttandee: true,
+        };
+      },
+      mocks: { $store: mockStore },
+    });
+
+  await wrapper.find(".joinButton").trigger("click");
+  await wrapper.vm.$nextTick();
+  expect(mockStore.dispatch).toHaveBeenCalledWith("addAttendee", data);
+});
+
+it("should not emit addAttendee if inputs are empty", async () => {
+  const mockStore = {
+      dispatch: jest.fn(),
     },
-    propsData:{
-      eventId: "111"
-    },
-    mocks: { $store: mockStore, },
-  });
+    wrapper = shallowMount(AddAttendee, {
+      computed: {
+        event: () => {
+          return event;
+        },
+      },
+      propsData: {
+        eventId: "111",
+      },
+      mocks: { $store: mockStore },
+    });
 
-
-await wrapper.find(".joinButton").trigger("click");
-await wrapper.vm.$nextTick();
-expect(mockStore.dispatch).not.toHaveBeenCalledWith("addAttendee", data);
-
-})  
+  await wrapper.find(".joinButton").trigger("click");
+  await wrapper.vm.$nextTick();
+  expect(mockStore.dispatch).not.toHaveBeenCalledWith("addAttendee", data);
+});
--- tests/unit/FishingEvent.spec.js
+++ tests/unit/FishingEvent.spec.js
@@ -1,27 +1,26 @@
-
 import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
-import FishingEvent from "@/views/FishingEvent.vue"
+import FishingEvent from "@/views/FishingEvent.vue";
 
 describe("FishingEvent", () => {
-
-let event, data;
-beforeEach(() => {
-    event = 
-        {
-            _id: "111",
-            date: "2020-10-30",
-            fish: "Gädda",
-            imgUrl: "lol",
-            info: "Jag har ett extra spö",
-            location: "Delsjön",
-            organizer: "Fredrik Berntsson",
-            time: "11:00",
-            comments: {commentName: "Allan", commentComment: "hej hej"},
-            attendees: {attendeeName: "Allan", AttendeeNr: "123"}
-          },
-    
-    data = {comment: { commentName: "hej", commentComment: "hej hej"}, id: "111"}
-})
+  let event, data;
+  beforeEach(() => {
+    (event = {
+      _id: "111",
+      date: "2020-10-30",
+      fish: "Gädda",
+      imgUrl: "lol",
+      info: "Jag har ett extra spö",
+      location: "Delsjön",
+      organizer: "Fredrik Berntsson",
+      time: "11:00",
+      comments: { commentName: "Allan", commentComment: "hej hej" },
+      attendees: { attendeeName: "Allan", AttendeeNr: "123" },
+    }),
+      (data = {
+        comment: { commentName: "hej", commentComment: "hej hej" },
+        id: "111",
+      });
+  });
 
   it("Should call addComment.", async () => {
     const mockStore = {
@@ -35,17 +34,15 @@ beforeEach(() => {
         },
         data() {
           return {
-            newComment: { commentName: "hej", commentComment: "hej hej"},
-            leaveComment: true
-          }
+            newComment: { commentName: "hej", commentComment: "hej hej" },
+            leaveComment: true,
+          };
         },
-        mocks: { $store: mockStore, },
+        mocks: { $store: mockStore },
       });
-    
+
     await wrapper.find("#sendComment").trigger("click");
     await wrapper.vm.$nextTick();
     expect(mockStore.dispatch).toHaveBeenCalledWith("addComment", data);
-  })   
-
-  
+  });
 });
